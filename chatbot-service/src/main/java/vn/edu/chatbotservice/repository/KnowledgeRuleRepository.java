551d620b6febe0bc4af0974e5222afeed64dc70e
Nguyễn Ngọc Danh <nguyenngocdanh92pch@gmail.com>
2026-06-06 10:13:59 +0700

    Delete chatbot-service directory

--- chatbot-service/src/main/java/vn/edu/chatbotservice/repository/KnowledgeRuleRepository.java
+++
@@ -1,9 +0,0 @@
-package vn.edu.chatbotservice.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import vn.edu.chatbotservice.entity.KnowledgeRule;
-
-@Repository
-public interface KnowledgeRuleRepository extends JpaRepository<KnowledgeRule, Long> {
-}
