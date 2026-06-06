4dc682629d418a5babbbc042957d7481d7c1e0d9
Nguyễn Ngọc Danh <nguyenngocdanh92pch@gmail.com>
2026-06-06 10:14:40 +0700

    Delete notification-service directory

--- notification-service/src/main/java/com/example/notification_service/repository/NotificationRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.notification_service.repository;
-
-import com.example.notification_service.entity.NotificationLog;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NotificationRepository extends JpaRepository<NotificationLog, Long> {
-}
