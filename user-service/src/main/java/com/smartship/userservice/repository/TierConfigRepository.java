2f020f722fc0cbb27881b1c08d9de018b7d82419
Nguyễn Ngọc Danh <nguyenngocdanh92pch@gmail.com>
2026-06-06 10:15:10 +0700

    Delete user-service directory

--- user-service/src/main/java/com/smartship/userservice/repository/TierConfigRepository.java
+++
@@ -1,9 +0,0 @@
-package com.smartship.userservice.repository;
-
-import com.smartship.userservice.entity.TierConfig;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TierConfigRepository extends JpaRepository<TierConfig, Long> {
-}
